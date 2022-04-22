4b08a3a58631491c30bfddd4a99e246ac383d144
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-04-22 15:10:31 +0200

    initialize Object properties

--- Object.hpp
+++ Object.hpp
@@ -6,8 +6,8 @@
 namespace kreogl {
     struct Object {
         const Model * model;
-        glm::mat4 transform;
-        glm::vec4 color;
-        glm::vec4 userData;
+        glm::mat4 transform{ 1.f };
+        glm::vec4 color { 1.f };
+        glm::vec4 userData{ 0.f };
     };
 }
