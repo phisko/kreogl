5862fd939feed0b2d9ed404b80be92ca1eb42343
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-05-25 12:02:33 +0200

    fix use of bad macro

--- kreogl/impl/kreogl.cpp
+++ kreogl/impl/kreogl.cpp
@@ -58,7 +58,7 @@ namespace kreogl {
             return false;
         }
 
-#ifndef _NDEBUG
+#ifndef NDEBUG
         glEnable(GL_DEBUG_OUTPUT);
         glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
         glDebugMessageCallback([](GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar * message, const void * userParam) {
