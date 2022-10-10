92ab78f3014d6bf06f1c0482daaf99c29d971f1e
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-10-10 15:31:21 +0200

    cleanup

--- kreogl/impl/shaders/shadowMap/PositionShadowMap/PositionShadowMapShader.hpp
+++ kreogl/impl/shaders/shadowMap/PositionShadowMap/PositionShadowMapShader.hpp
@@ -8,11 +8,11 @@ namespace kreogl {
     class PositionShadowMapShader : public ShadowMapShader, public Singleton<PositionShadowMapShader> {
 	public:
         PositionShadowMapShader() noexcept;
-        void drawToTexture(GLuint texture, const glm::mat4 & lightSpaceMatrix, const DrawParams & params) noexcept override;
 
     private:
         void addSourceFiles() noexcept override;
         std::vector<UniformBase *> getUniforms() noexcept override;
+        void drawToTexture(GLuint texture, const glm::mat4 & lightSpaceMatrix, const DrawParams & params) noexcept override;
 
     private:
         PositionProjViewModelGLSL _glsl;
