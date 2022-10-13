6274fec1d4ad2170bfa853c64551cd2a3c876b9d
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-10-13 15:09:40 +0200

    add shapes::Box helper

---
+++ kreogl/shapes/Box.cpp
@@ -0,0 +1,67 @@
+#include "Box.hpp"
+
+#include <GL/glew.h>
+
+namespace kreogl::shapes {
+    void drawBox() noexcept {
+        static GLuint vao = -1;
+        if (vao == -1) {
+            static constexpr float vertices[] = {
+                // positions
+                -.5f,  .5f, -.5f,
+                -.5f, -.5f, -.5f,
+                .5f, -.5f, -.5f,
+                .5f, -.5f, -.5f,
+                .5f,  .5f, -.5f,
+                -.5f,  .5f, -.5f,
+
+                -.5f, -.5f,  .5f,
+                -.5f, -.5f, -.5f,
+                -.5f,  .5f, -.5f,
+                -.5f,  .5f, -.5f,
+                -.5f,  .5f,  .5f,
+                -.5f, -.5f,  .5f,
+
+                .5f, -.5f, -.5f,
+                .5f, -.5f,  .5f,
+                .5f,  .5f,  .5f,
+                .5f,  .5f,  .5f,
+                .5f,  .5f, -.5f,
+                .5f, -.5f, -.5f,
+
+                -.5f, -.5f,  .5f,
+                -.5f,  .5f,  .5f,
+                .5f,  .5f,  .5f,
+                .5f,  .5f,  .5f,
+                .5f, -.5f,  .5f,
+                -.5f, -.5f,  .5f,
+
+                -.5f,  .5f, -.5f,
+                .5f,  .5f, -.5f,
+                .5f,  .5f,  .5f,
+                .5f,  .5f,  .5f,
+                -.5f,  .5f,  .5f,
+                -.5f,  .5f, -.5f,
+
+                -.5f, -.5f, -.5f,
+                -.5f, -.5f,  .5f,
+                .5f, -.5f, -.5f,
+                .5f, -.5f, -.5f,
+                -.5f, -.5f,  .5f,
+                .5f, -.5f,  .5f
+            };
+
+            glGenVertexArrays(1, &vao);
+            GLuint vbo;
+            glGenBuffers(1, &vbo);
+            glBindVertexArray(vao);
+            glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+            glEnableVertexAttribArray(0);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        }
+
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+}
