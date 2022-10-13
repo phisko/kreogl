64101872910961b13c36aeb4dba9529816789dc0
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-10-13 15:09:29 +0200

    add shapes::Line helper

---
+++ kreogl/shapes/Line.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace kreogl::shapes {
+    void drawLine(const glm::vec3 & start, const glm::vec3 & end) noexcept;
+}
---
+++ kreogl/shapes/Line.md
@@ -0,0 +1,7 @@
+# [Line](Line.hpp)
+
+```cpp
+void drawLine(const glm::vec3 & start, const glm::vec3 & end) noexcept;
+```
+
+Draw a line from `start` to `end`.
