67cfdc0fa8eea21dfcbcbc2c2ef92d32591ea903
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-10-17 11:04:21 +0200

    add shapes::drawTexturedQuad

---
+++ kreogl/shapes/TexturedQuad.cpp
@@ -0,0 +1,34 @@
+#include "TexturedQuad.hpp"
+
+#include <GL/glew.h>
+
+namespace kreogl::shapes {
+    void drawTexturedQuad() noexcept {
+        static GLuint vao = 0;
+        static GLuint vbo;
+
+        if (vao == 0) {
+            const float vertices[] = {
+                // positions			// texCoords
+                -1.0f, -1.0f, 0.0f,		0.f, 1.f,
+                -1.0f, 1.0f, 0.0f,		0.f, 0.f,
+                1.0f, -1.0f, 0.0f,		1.f, 1.f,
+                1.0f, 1.0f, 0.0f,		1.f, 0.f,
+            };
+            // setup plane VAO
+            glGenVertexArrays(1, &vao);
+            glGenBuffers(1, &vbo);
+            glBindVertexArray(vao);
+            glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
+            glEnableVertexAttribArray(0);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), nullptr);
+            glEnableVertexAttribArray(1);
+            glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+        }
+
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        glBindVertexArray(0);
+    }
+}
