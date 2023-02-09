9d5756d2479162225293e7795dec54bb035b889e
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-02-09 09:45:27 +0100

    fix specular texture being read as long as diffuse texture existed

--- kreogl/impl/shaders/helpers/skeletal/skeletal_glsl.hpp
+++ kreogl/impl/shaders/helpers/skeletal/skeletal_glsl.hpp
@@ -16,8 +16,9 @@ namespace kreogl {
 		uniform<glm::mat4> bones = { "bones" }; // [KREOGL_SKELETON_MAX_BONES] but we'll only use the first and do a single upload
 
 		static const char * frag;
-		uniform<bool> has_texture = { "has_texture" };
+		uniform<bool> has_diffuse_texture = { "has_diffuse_texture" };
 		uniform<size_t> texture_diffuse = { "texture_diffuse" };
+		uniform<bool> has_specular_texture = { "has_specular_texture" };
 		uniform<size_t> texture_specular = { "texture_specular" };
 		uniform<glm::vec4> diffuse_color = { "diffuse_color" };
 		uniform<glm::vec4> specular_color = { "specular_color" };
